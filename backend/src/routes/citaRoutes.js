a58754cbc73746d1161e36d60624bf21a78329fe
Tzionao <98960090+Tzionao@users.noreply.github.com>
2026-06-03 11:24:29 -0500

    Add route to get citas by paciente ID
    Se agregó ruta para obtener las citas de cada paciente, con base en  ID de paciente

--- backend/src/routes/citaRoutes.js
+++ backend/src/routes/citaRoutes.js
@@ -4,6 +4,7 @@ const citaController = require('../controllers/citaController');
 // Rutas para las citas
 router.get('/', citaController.getAllCitas);
 router.post('/', citaController.createCita);
+router.get('/paciente/:id', citaController.getCitasByPaciente);
 router.put('/:id', citaController.updateCita);
 router.delete('/:id', citaController.deleteCita);
 module.exports = router;
