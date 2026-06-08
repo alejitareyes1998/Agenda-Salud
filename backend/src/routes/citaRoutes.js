5890dd8f81ab281d947c39e57e070bd9234ec51b
Tzionao <98960090+Tzionao@users.noreply.github.com>
2026-06-08 13:41:31 -0500

    Add route to obtain availability in 20-minute blocks
    Agregue una ruta para obtener disponibilidad en bloques de 20 minutos.

--- backend/src/routes/citaRoutes.js
+++ backend/src/routes/citaRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const citaController = require('../controllers/citaController');
 // Rutas para las citas
 router.get('/', citaController.getAllCitas);
+// === se agregó esta NUEVA RUTA: para obtener disponibilidad en bloques de 20 min ===
+router.get('/disponibilidad', citaController.obtenerDisponibilidad);
+
 router.post('/', citaController.createCita);
 router.get('/paciente/:id', citaController.getCitasByPaciente);
 router.put('/:id', citaController.updateCita);
